f940f7e63f3111570429ec45237fcaa5c2f5c04a
sistem-ux <sistema.bareca@gmail.com>
2026-08-01 16:47:30 -0400

    clave

--- supabase-config.example.js
+++ supabase-config.example.js
@@ -2,8 +2,8 @@
 // Do NOT commit your real `supabase-config.js` to source control.
 
 // Example:
-// export const SUPABASE_URL = 'https://cibtpkpxdrykozujaqba.supabase.co';
-// export const SUPABASE_ANON_KEY = 'sb_publishable_zlp4_HGpTeAQKW55c_pZQA_2HEXRpaC';
+// export const SUPABASE_URL = 'https://xyzcompany.supabase.co';
+// export const SUPABASE_ANON_KEY = 'public-anon-key';
 
-export const SUPABASE_URL = '';
-export const SUPABASE_ANON_KEY = ''; 
+export const SUPABASE_URL = 'https://cibtpkpxdrykozujaqba.supabase.co/rest/v1/';
+export const SUPABASE_ANON_KEY = 'sb_publishable_zlp4_HGpTeAQKW55c_pZQA_2HEXRpaC'; 
